0f187f238368b47745592b2439b1b62e110c711d
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-07-05 16:53:25 -0700

    add functions to index.js and work through testing, WIP

--- src/gameSetup.js
+++ src/gameSetup.js
@@ -1,42 +1,38 @@
-export default class gameStartLogic {
-  deck(){
-    function card(value, name, suit) {
-      this.value = value;
-      this.name = name;
-      this.suit = suit;
-    }
-    // let card = (this.value, this.name, this.suit);
-    this.names = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-    this.jokers = 'Joker'; //do jokers need to be individually named?
-    this.suits = ['Hearts','Diamonds','Spades','Clubs'];
-    const cards = [];
-      
-    for( let s = 0; s < this.suits.length; s++ ) {
-      for( let n = 0; n < this.names.length; n++ ) {
-        cards.push(new card( n+1, this.names[n], this.suits[s] ) );
+export function deck(cards) {
+  function card(value, name, suit) {
+    this.value = value;
+    this.name = name;
+    this.suit = suit;
+    this.name = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+    this.joker = 'Joker'; //do jokers need to be individually named?
+    this.suit = ['Hearts','Diamonds','Spades','Clubs'];
+    cards = [];
+  
+    for( let s = 0; s < this.suit.length; s++ ) {
+      for( let n = 0; n < this.name.length; n++ ) {
+        cards.push(new card( n+1, this.name[n], this.suit[s] ) );
       }
     }
-    cards.push(this.jokers); //does Jokers get added to cards array?
+    cards.push(this.joker); //does Jokers get added to cards array?
+    console.log(cards);
     return cards;
-  }
-
-  shuffle(o) {
-    for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-    return o;
-  }
-
-  deal(myDeck) {
-    let userHand = [];
-    let card1 = myDeck.pop();
-    let card2 = myDeck.pop();
-    let card3 = myDeck.pop();
-    let card4 = myDeck.pop();
-    userHand.push(card1, card2, card3, card4);
-    return userHand;
   }
 }
 
+export function shuffle(o) {
+  for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+  return o;
+}
 
+export function deal(myDeck) {
+  let userHand = [];
+  let card1 = myDeck.pop();
+  let card2 = myDeck.pop();
+  let card3 = myDeck.pop();
+  let card4 = myDeck.pop();
+  userHand.push(card1, card2, card3, card4);
+  return userHand;
+}
 
 // 1. establish 52 card deck [x]
 // 2. establish function to shuffle 52 card deck [x]
--- src/index.js
+++ src/index.js
@@ -2,29 +2,36 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
-import gameStartLogic from './gameSetup.js';
-import gamePlay from './gamePlay.js';
+// import gameStartLogic from './gameSetup.js';
+import {gamePlay} from './gamePlay.js';
+import {deck} from './gameSetup.js';
+import {shuffle} from './gameSetup.js';
+import {deal} from './gameSetup.js';
 
-let myDeck = new deck();
-myDeck = shuffle(myDeck);
-let newHand = deal(myDeck);
-let gameTurn = gamePlay(myDeck);
-console.log(gameTurn);
-console.log(newHand);
-console.log(myDeck);
+$('#gameStartBtn').click(function() {
+  let userHand = [];
+  let inPlay;
+  let myDeck = deck();
+  myDeck = shuffle(myDeck);
+  userHand = deal(myDeck);
+  inPlay = gamePlay(myDeck);
+  console.log(userHand);
+  console.log(inPlay);
+  console.log(myDeck);
+});
 
-for(var i=0; i < myDeck.length; i++){
-  div = document.createElement('div');
-  div.className = 'card';
+// for(var i=0; i < myDeck.length; i++){
+//   div = document.createElement('div');
+//   div.className = 'card';
 
-  if(myDeck[i].suit == 'Diamonds'){
-    var ascii_char = '♦';
-  } else {
-    var ascii_char = '&' + myDeck[i].suit.toLowerCase() + ';';
-  }
+//   if(myDeck[i].suit == 'Diamonds'){
+//     var ascii_char = '♦';
+//   } else {
+//     var ascii_char = '&' + myDeck[i].suit.toLowerCase() + ';';
+//   }
 
-  div.innerHTML = '' + myDeck[i].name + '' + ascii_char + '';
-  document.body.appendChild(div);
-}
+//   div.innerHTML = '' + myDeck[i].name + '' + ascii_char + '';
+//   document.body.appendChild(div);
+// }
 
 // import { Object } from './businesslog.js';
