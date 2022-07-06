62bdda737394c5db3259d775af19ad3da9e72f3a
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-07-06 15:45:05 -0700

    WIP on renderCard.js

--- src/renderCard.js
+++ src/renderCard.js
@@ -2,20 +2,20 @@ function renderCard(userHand, inPlay)
 {
   // document.getElementById('deck').innerHTML = '';
 
-  for(var i = 0; i < userHand.length; i++)
+  for(var i = 0; i < inPlay.length; i++)
   {
-    // let card = document.createElement("div");
-    // var icon = '';
-    if (userHand[i].suit == 'hearts')
+    let card = inPlayCard.createElement("div"); //identify where card needs to be created 
+    let icon = '';
+    if (inPlay[i].suit == 'hearts')
       icon='&hearts';
-    else if (deck[i].Suit == 'spades')
+    else if (inPlay[i].Suit == 'spades')
       icon = '&spades';
-    else if (deck[i].Suit == 'diamonds')
+    else if (inPlay[i].Suit == 'diamonds')
       icon = '&diams';
     else
       icon = '&clubs';
 
-    card.innerHTML = deck[i].Value + '' + icon;
+    card.innerHTML = inPlay[i].Value + '' + icon;
     card.className = 'card';
     document.getElementById("deck").appendChild(card);
   }
